6b30405d669200ecc3abf8e8cf79ded465c8072b
Markus Schmitz <18517685+markusschmitz53@users.noreply.github.com>
2020-07-05 20:34:56 +0200

    Added RadListView

--- app/app.js
+++ app/app.js
@@ -2,6 +2,9 @@ import Vue from "nativescript-vue";
 import App from "./components/App";
 import firebase from 'nativescript-plugin-firebase';
 import { RangeSeekBar } from "nativescript-range-seek-bar/range-seek-bar";
+import RadListView from 'nativescript-ui-listview/vue';
+
+Vue.use(RadListView);
 
 firebase.init({
   // Optionally pass in properties for database, authentication and cloud messaging,
